635137e2c56e3094ca18a0300ed40d61c03bfceb
Alexandre Capt <acapt@adobe.com>
2022-12-08 13:44:04 +0100

    feat: import first shot

---
+++ tools/importer/import.js
@@ -0,0 +1,88 @@
+/*
+ * Copyright 2022 Adobe. All rights reserved.
+ * This file is licensed to you under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License. You may obtain a copy
+ * of the License at http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under
+ * the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR REPRESENTATIONS
+ * OF ANY KIND, either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+/* global WebImporter */
+/* eslint-disable no-console, class-methods-use-this */
+
+const makeProxySrcs = (main, host) => {
+  main.querySelectorAll('img').forEach((img) => {
+    if (img.src.startsWith('/')) {
+      // make absolute
+      const cu = new URL(host);
+      img.src = `${cu.origin}${img.src}`;
+    }
+    try {
+      const u = new URL(img.src);
+      u.searchParams.append('host', u.origin);
+      img.src = `http://localhost:3001${u.pathname}${u.search}`;
+    } catch (error) {
+      console.warn(`Unable to make proxy src for ${img.src}: ${error.message}`);
+    }
+  });
+};
+
+export default {
+  /**
+   * Apply DOM operations to the provided document and return
+   * the root element to be then transformed to Markdown.
+   * @param {HTMLDocument} document The document
+   * @param {string} url The url of the page imported
+   * @param {string} html The raw html (the document is cleaned up during preprocessing)
+   * @param {object} params Object containing some parameters given by the import process.
+   * @returns {HTMLElement} The root element to be transformed
+   */
+  transformDOM: ({
+    // eslint-disable-next-line no-unused-vars
+    document, url, html, params,
+  }) => {
+    // use helper method to remove header, footer, etc.
+    WebImporter.DOMUtils.remove(document.body, [
+      '.navbar',
+      '.leftnav-wrapper',
+      '.ccdHeader',
+      '#footer',
+      '.platform-tabs',
+      '.information',
+      '.action-bar',
+    ]);
+
+    const bckimg = document.querySelectorAll('[style*="background-image"]');
+    bckimg.forEach((e) => {
+      WebImporter.DOMUtils.replaceBackgroundByImg(e, document);
+    });
+
+    // promote title to h1
+    const name = document.querySelector('.product-name')
+    if (name) {
+      const h1 = document.createElement('h1');
+      h1.innerHTML = name.innerHTML;
+      name.replaceWith(h1);
+    }
+
+    makeProxySrcs(document.body, ' https://odin.adobe.com');
+
+    return document.body;
+  },
+
+  /**
+   * Return a path that describes the document being transformed (file name, nesting...).
+   * The path is then used to create the corresponding Word document.
+   * @param {HTMLDocument} document The document
+   * @param {string} url The url of the page imported
+   * @param {string} html The raw html (the document is cleaned up during preprocessing)
+   * @param {object} params Object containing some parameters given by the import process.
+   * @return {string} The path
+   */
+  generateDocumentPath: ({
+    // eslint-disable-next-line no-unused-vars
+    document, url, html, params,
+  }) => new URL(url).pathname.replace(/\.html$/, '').replace(/\/$/, ''),
+};
